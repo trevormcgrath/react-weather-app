d4b8a51babb7ec16e7ca6b457c6a0b33bfefd011
Trevor McGrath <trevormcgrath88@gmail.com>
2016-09-06 14:52:56 -0700

    Testing autodeployment to Heroku final test

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,7 +7,7 @@ var Nav = () => {
 			<h2>Nav Component</h2>
 			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
 			<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples 1</Link>
+			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
 		</div>
 	);
 }
